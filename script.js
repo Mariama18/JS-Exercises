cc7fc1bbb47fc8ec36e99bc27fbcd8ae7c254bf9
Mariama18 <39253609+Mariama18@users.noreply.github.com>
2018-05-21 10:45:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /* JavaScript code here */
+console.log("External JS")
+
+//Task 2
+function sumArray(arr){
+    var sum=0;
+    arr.forEach(function(value, index){
+        sum+=value;
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([1,20,-3,45]));
+
+//Task 3
+function checkEmail(emailString){
+    var emailFormat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match=emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith.com"));
